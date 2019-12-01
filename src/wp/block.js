b5f35ee76dce4a8cc34b7f9960d39e2a8cfc0149
Matt Pilott <matt@creativelittledots.co.uk>
2019-12-01 10:37:24 +0000

    improved efficiency of block function

--- src/wp/block.js
+++ src/wp/block.js
@@ -1,15 +1,14 @@
 /**
  * Returns a component name based on a lazyblock slug
  * @memberof WordPress
- * @version 1.0.0
+ * @version 1.1.0
  * @param {object} block the block object
  * @returns {string} the component name for use in <svelte:component>
  */
 
 export default function(block) {
    block = (block || '')
-      .replace('lazyblock/', '')
-      .replace('core/', '')
+      .replace(/(lazyblock\/|core\/)/g, '')
       .toLowerCase()
       .replace(/(\b|-)\w/g, m => m.toUpperCase().replace(/-/, ''));
 
