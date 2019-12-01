9d623f5a24183461f1da4a0a6ea09baa6b21298c
Matt Pilott <matt@creativelittledots.co.uk>
2019-12-01 10:42:17 +0000

    getObject test

---
+++ cypress/integration/getObject.js
@@ -0,0 +1,9 @@
+import getObject from '../../src/data/getObject';
+
+describe('Modules', function(){
+
+   it('Can correctly run getObject and return the desired object', function() {
+      const arr = [{ testA: 1, testB: 2 }, { testA: 3, testB: 4 }]
+      expect(getObject(arr, 'testA', 3)).to.eq(arr[1])
+   })
+})
