77614e84bb43486f8ca42e96c08d8579182cb656
Matt Pilott <matt@creativelittledots.co.uk>
2019-12-30 16:07:39 +0000

    dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,39 @@
 import babel from 'rollup-plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
 import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
+import 'dotenv/config';
 
+const api_url = process.env.API_URL;
 const plugins = [
    babel({
       exclude: [
          /\/core-js\//,
+         'dist/**',
          'node_modules/**',
          'testing/**'
       ],
       presets: [[ '@babel/env' ]]
    }),
    commonjs(),
-   resolve()
+   replace({
+      'process.env.API_URL': JSON.stringify(api_url),
+   }),
+   resolve(),
+   terser()
 ];
 
 export default [
+   {
+      input: './src/index.js',
+      output: {
+         file: './dist/bundle.js',
+         format: 'umd',
+         name: 'NeueKit'
+      },
+      plugins
+   },
    {
       input: './testing/index.js',
       output: {
--- src/svelte/pull.js
+++ src/svelte/pull.js
@@ -18,7 +18,7 @@ export default async function(
    callback
 ) {
    const args = Object.keys(params).length ? '?' + encode(params) : '';
-   const url = endpoint + args;
+   const url = process.env.API_URL ? process.env.API_URL + endpoint + args : endpoint + args;
 
    const req = await (preload ? preload.fetch(url) : fetch(url));
 
