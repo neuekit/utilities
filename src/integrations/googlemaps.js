9d7532c062e67fc0f734cf652d4c9a297238dddc
Matt Pilott <matt@creativelittledots.co.uk>
2020-11-23 21:12:56 +0000

    fix for googlemaps

--- src/integrations/googlemaps.js
+++ src/integrations/googlemaps.js
@@ -4,16 +4,18 @@ import { encode } from 'qss';
 /**
  * Allows for programmatic insertion of Google Maps script
  * @memberof Integrations
- * @version 1.0.0
+ * @version 1.1.0
  * @param {object} options the Google Maps Api params
  */
 export default function (options = {}) {
-   if (document.getElementById('neuekitGoogle')) {
-      window[options.callback]();
+   let script = document.getElementById('neuekitGoogle');
+   
+   if (script) {
+      script.onload = window[options.callback];
       return;
    }
    
-   const script = createEl('script', {
+   script = createEl('script', {
       src: 'https://maps.googleapis.com/maps/api/js?' + encode(options),
       id: 'neuekitGoogle',
       async: '',
