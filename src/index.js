542faad04039f3876debb20f3bd462c108006ad8
Matt Pilott <matt@creativelittledots.co.uk>
2021-01-02 20:44:45 +0000

    remove wordpress helpers

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import * as integrations from './integrations';
 import * as middleware from './middleware';
 import * as svelte from './svelte';
 import * as vanilla from './vanilla';
-import * as wordpress from './wordpress';
 
 export {
    integrations,
    middleware,
    svelte,
-   vanilla,
-   wordpress
+   vanilla
 }
