8d4b861f6b5717c2911c7e017020470e9f1e25e5
Matt Pilott <matt@creativelittledots.co.uk>
2021-01-02 20:46:17 +0000

    implement uvu test suite

---
+++ sandbox/index.js
@@ -0,0 +1,6 @@
+// import { analytics } from '../src/integrations/index.js';
+// import { sanslash } from '../src/middleware/index.js';
+// import { pull } from '../src/svelte/index.js';
+import { getObject } from '../src/vanilla/index.js';
+
+console.log(getObject);
