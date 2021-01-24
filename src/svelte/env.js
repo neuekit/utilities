34a047a278e9c9b62f9e02b25fc03821dee909df
Matt Pilott <matt@creativelittledots.co.uk>
2021-01-24 19:36:41 +0000

    sapper env modules

---
+++ src/svelte/env.js
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+
+/**
+ * Svelte/Sapper automatic environment variable array
+ * @memberof Svelte
+ * @version 1.0.0
+ * @param {string} prefix
+ * @returns {object} environment variables for replacement
+ */
+
+export default function(prefix = 'SS_') {
+   const obj = {}
+   
+   for (let item in process.env) {
+      if(item.startsWith(prefix) ) {
+         obj[item] = String(process.env[item])
+      }
+   }
+   
+   return obj
+}
--- src/svelte/index.js
+++ src/svelte/index.js
@@ -2,6 +2,7 @@
  * Svelte modules
  * @namespace Svelte
  */
+export { default as env } from './env'
 export { default as pull } from './pull'
 export { default as push } from './push'
 export { default as storable } from './storable'
